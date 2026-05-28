53988a4da44e208f10a5234ed1818258d45b5def
aakumykov <aakumykov@yandex.ru>
2026-05-28 11:48:50 +0300

    0

---
+++ app/src/androidTest/java/com/github/aakumykov/seek_bar_with_text_input_project/ExampleInstrumentedTest.kt
@@ -0,0 +1,27 @@
+package com.github.aakumykov.seek_bar_with_text_input_project
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals(
+            "com.github.aakumykov.seek_bar_with_text_input_project",
+            appContext.packageName
+        )
+    }
+}
